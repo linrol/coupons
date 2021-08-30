7303456a625284c90bad877945ddc7fc6d82fcba
罗林 <1071893649@qq.com>
2021-08-31 03:15:17 +0800

    还原

--- server/src/main/java/com/huaan9527/mall/webapi/configuration/SecurityConfiguration.java
+++ server/src/main/java/com/huaan9527/mall/webapi/configuration/SecurityConfiguration.java
@@ -21,7 +21,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.apply(new SocialConfigurer());
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/coupon/api/st/**", "/coupon/api/mi/**", "/coupon/api/auth/**", "/coupon/api/favicon.ico", "/coupon/api/login", "/coupon/api/logout")
+                .antMatchers("/st/**", "/mi/**", "/auth/**", "/favicon.ico", "/login", "/logout")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
